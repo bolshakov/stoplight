a4515eeb7637ea39d96857a5a0e8ea8400b5d6c4
Taylor Fausak <taylor@fausak.me>
2014-08-04 18:39:40 -0500

    Create #time

--- lib/stoplight/failure.rb
+++ lib/stoplight/failure.rb
@@ -2,5 +2,11 @@
 
 module Stoplight
   class Failure
+    # @return [Time]
+    attr_reader :time
+
+    def initialize
+      @time = Time.now
+    end
   end
 end
--- spec/lib/stoplight/failure_spec.rb
+++ spec/lib/stoplight/failure_spec.rb
@@ -4,4 +4,18 @@ require 'spec_helper'
 
 describe Stoplight::Failure do
   subject(:failure) { described_class.new }
+
+  describe '#initialize' do
+    it 'assigns @time' do
+      expect(failure.instance_variable_get(:@time)).to be_within(1).of(Time.now)
+    end
+  end
+
+  describe '#time' do
+    subject(:result) { failure.time }
+
+    it 'returns the time' do
+      expect(result).to be_within(1).of(Time.now)
+    end
+  end
 end
