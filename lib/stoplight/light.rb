83c7e1e45b179cb5206e17c1afdda328b6937b4b
Taylor Fausak <taylor@fausak.me>
2014-08-04 17:47:37 -0500

    Create #with_code

--- lib/stoplight/light.rb
+++ lib/stoplight/light.rb
@@ -2,5 +2,9 @@
 
 module Stoplight
   class Light
+    def with_code(&block)
+      @code = block
+      self
+    end
   end
 end
--- spec/lib/stoplight/light_spec.rb
+++ spec/lib/stoplight/light_spec.rb
@@ -4,4 +4,18 @@ require 'spec_helper'
 
 describe Stoplight::Light do
   subject(:light) { described_class.new }
+
+  describe '#with_code' do
+    let(:block) { proc {} }
+
+    subject(:result) { light.with_code(&block) }
+
+    it 'returns self' do
+      expect(result).to equal(light)
+    end
+
+    it 'assigns @code' do
+      expect(result.instance_variable_get(:@code)).to eql(block)
+    end
+  end
 end
