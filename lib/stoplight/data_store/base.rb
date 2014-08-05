07a6d85aaddf39773025291878ad51914a87711c
Taylor Fausak <taylor@fausak.me>
2014-08-04 19:13:38 -0500

    Create #read

--- lib/stoplight/data_store/base.rb
+++ lib/stoplight/data_store/base.rb
@@ -2,7 +2,13 @@
 
 module Stoplight
   module DataStore
+    # @abstract
     class Base
+      # @param key [Object]
+      # @return [Object, nil]
+      def read(_key)
+        fail NotImplementedError
+      end
     end
   end
 end
--- spec/lib/stoplight/data_store/base_spec.rb
+++ spec/lib/stoplight/data_store/base_spec.rb
@@ -4,4 +4,14 @@ require 'spec_helper'
 
 describe Stoplight::DataStore::Base do
   subject(:data_store) { described_class.new }
+
+  describe '#read' do
+    let(:key) { SecureRandom.hex }
+
+    subject(:result) { data_store.read(key) }
+
+    it 'raises an error' do
+      expect { result }.to raise_error(NotImplementedError)
+    end
+  end
 end
