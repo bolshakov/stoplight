818ce9843fe32423c6b44df004dee4e4813b861a
Taylor Fausak <taylor@fausak.me>
2014-08-13 14:11:11 -0500

    Use forwarded data store methods

--- lib/stoplight/light.rb
+++ lib/stoplight/light.rb
@@ -55,7 +55,7 @@ module Stoplight
     # @param threshold [Integer]
     # @return [self]
     def with_threshold(threshold)
-      Stoplight.data_store.set_threshold(name, threshold.to_i)
+      Stoplight.set_threshold(name, threshold.to_i)
       self
     end
 
@@ -91,25 +91,25 @@ module Stoplight
 
     def run_code
       result = code.call
-      Stoplight.data_store.clear_failures(name)
+      Stoplight.clear_failures(name)
       result
     rescue => error
       if error_allowed?(error)
-        Stoplight.data_store.clear_failures(name)
+        Stoplight.clear_failures(name)
       else
-        Stoplight.data_store.record_failure(name, error)
+        Stoplight.record_failure(name, error)
       end
 
       raise
     end
 
     def run_fallback
-      Stoplight.data_store.record_attempt(name)
+      Stoplight.record_attempt(name)
       fallback.call
     end
 
     def sync_settings
-      Stoplight.data_store.set_threshold(name, threshold)
+      Stoplight.set_threshold(name, threshold)
     end
   end
 end
