54459cf44458f8bd93e18b489463d314a4715ecb
Taylor Fausak <taylor@fausak.me>
2014-08-28 08:40:05 -0500

    Benchmark Redis data store

---
+++ bench/stoplight_redis_bench.rb
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+require 'benchmark/ips'
+require 'fakeredis'
+require 'stoplight'
+
+redis = Redis.new
+data_store = Stoplight::DataStore::Redis.new(redis)
+Stoplight.data_store = data_store
+
+Benchmark.ips do |b|
+  b.report('creating lambda')    { -> {} }
+  b.report('calling lambda')     { -> {}.call }
+  b.report('creating stoplight') { Stoplight::Light.new('') {} }
+  b.report('calling stoplight')  { Stoplight::Light.new('') {}.run }
+
+  b.compare!
+end
