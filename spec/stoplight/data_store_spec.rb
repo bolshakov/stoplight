87338a1f62fe30d250557f9c132e7655ff4d9ad5
Taylor Fausak <taylor@fausak.me>
2014-08-28 15:34:27 -0500

    Test data store validation

---
+++ spec/stoplight/data_store_spec.rb
@@ -0,0 +1,60 @@
+# coding: utf-8
+
+require 'spec_helper'
+
+describe Stoplight::DataStore do
+  describe '.validate_color!' do
+    subject(:result) { described_class.validate_color!(color) }
+    let(:color) { nil }
+
+    context 'with an invalid color' do
+      it 'raises an error' do
+        expect { result }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '.validate_failure!' do
+    subject(:result) { described_class.validate_failure!(failure) }
+    let(:failure) { nil }
+
+    context 'with an invalid failure' do
+      it 'raises an error' do
+        expect { result }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '.validate_state!' do
+    subject(:result) { described_class.validate_state!(state) }
+    let(:state) { nil }
+
+    context 'with an invalid state' do
+      it 'raises an error' do
+        expect { result }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '.validate_threshold!' do
+    subject(:result) { described_class.validate_threshold!(threshold) }
+    let(:threshold) { nil }
+
+    context 'with an invalid threshold' do
+      it 'raises an error' do
+        expect { result }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '.validate_timeout!' do
+    subject(:result) { described_class.validate_timeout!(timeout) }
+    let(:timeout) { nil }
+
+    context 'with an invalid timeout' do
+      it 'raises an error' do
+        expect { result }.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
