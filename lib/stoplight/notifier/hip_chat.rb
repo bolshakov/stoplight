b9c24406ce348f477a9ee87b8d655ccbb739dce9
Taylor Fausak <taylor@fausak.me>
2014-08-28 09:16:07 -0500

    Fix #32; add format to HipChat notifier

--- lib/stoplight/notifier/hip_chat.rb
+++ lib/stoplight/notifier/hip_chat.rb
@@ -4,19 +4,21 @@ module Stoplight
   module Notifier
     # @note hipchat ~> 1.3.0
     class HipChat < Base
+      DEFAULT_FORMAT = '@all %s'
       DEFAULT_OPTIONS = { color: 'red', message_format: 'text', notify: true }
 
       # @param client [HipChat::Client]
       # @param room [String]
       # @param options [Hash]
-      def initialize(client, room, options = {})
+      def initialize(client, room, format = nil, options = {})
         @client = client
         @room = room
+        @format = format || DEFAULT_FORMAT
         @options = DEFAULT_OPTIONS.merge(options)
       end
 
       def notify(message)
-        @client[@room].send('Stoplight', "@all #{message}", @options)
+        @client[@room].send('Stoplight', @format % message, @options)
       end
     end
   end
