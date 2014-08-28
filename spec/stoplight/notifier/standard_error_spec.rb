4928d388c3bfb1ea243128dd5832edc38e3df1eb
Taylor Fausak <taylor@fausak.me>
2014-08-28 09:06:18 -0500

    Actually check STDERR instead of mocking

--- spec/stoplight/notifier/standard_error_spec.rb
+++ spec/stoplight/notifier/standard_error_spec.rb
@@ -5,14 +5,17 @@ require 'spec_helper'
 describe Stoplight::Notifier::StandardError do
   subject(:notifier) { described_class.new }
 
+  before { @stderr, $stderr = $stderr, StringIO.new }
+  after { $stderr = @stderr }
+
   describe '#notify' do
     let(:message) { SecureRandom.hex }
 
     subject(:result) { notifier.notify(message) }
 
     it 'emits the message as a warning' do
-      expect(notifier).to receive(:warn).with(message)
       result
+      expect($stderr.string).to eql("#{message}\n")
     end
   end
 end
