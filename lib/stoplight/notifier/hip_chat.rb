9a07856ef4e24d0c018ca87896c19ef76f1c1f91
Taylor Fausak <taylor@fausak.me>
2014-09-15 17:08:43 -0500

    Fix #47; generate notifications with lambdas

--- lib/stoplight/notifier/hip_chat.rb
+++ lib/stoplight/notifier/hip_chat.rb
@@ -4,21 +4,24 @@ module Stoplight
   module Notifier
     # @note hipchat ~> 1.3.0
     class HipChat < Base
-      DEFAULT_FORMAT = '@all Switching %s from %s to %s'
+      DEFAULT_FORMATTER = lambda do |light, from_color, to_color|
+        "@all Switching #{light.name} from #{from_color} to #{to_color}"
+      end
       DEFAULT_OPTIONS = { color: 'red', message_format: 'text', notify: true }
 
       # @param client [HipChat::Client]
       # @param room [String]
+      # @param formatter [Proc, nil]
       # @param options [Hash]
-      def initialize(client, room, format = nil, options = {})
+      def initialize(client, room, formatter = nil, options = {})
         @client = client
         @room = room
-        @format = format || DEFAULT_FORMAT
+        @formatter = formatter || DEFAULT_FORMATTER
         @options = DEFAULT_OPTIONS.merge(options)
       end
 
       def notify(light, from_color, to_color)
-        message = format(@format, light.name, from_color, to_color)
+        message = @formatter.call(light, from_color, to_color)
         @client[@room].send('Stoplight', message, @options)
       end
     end
