a058ce2ddaa710fd91df8801983bc2162175be3e
Taylor Fausak <taylor@fausak.me>
2015-02-04 22:43:35 -0600

    Create a helper function for creating stoplights

--- spec/stoplight_spec.rb
+++ spec/stoplight_spec.rb
@@ -7,3 +7,15 @@ describe Stoplight do
     expect(described_class).to be_a(Module)
   end
 end
+
+describe 'Stoplight' do
+  subject(:light) { Stoplight(name, &code) }
+  let(:name) { ('a'..'z').to_a.shuffle.join }
+  let(:code) { -> {} }
+
+  it 'creates a stoplight' do
+    expect(light).to be_a(Stoplight::Light)
+    expect(light.name).to eql(name)
+    expect(light.code).to eql(code)
+  end
+end
