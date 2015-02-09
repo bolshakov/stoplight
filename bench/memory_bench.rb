c7772959a2faa4f43ab11b4e96f84a3d8897026d
Taylor Fausak <taylor@fausak.me>
2015-02-09 15:39:55 -0600

    Update benchmarks to use shortcut

--- bench/memory_bench.rb
+++ bench/memory_bench.rb
@@ -6,8 +6,8 @@ require 'stoplight'
 Benchmark.ips do |b|
   b.report('creating lambda')    { -> {} }
   b.report('calling lambda')     { -> {}.call }
-  b.report('creating stoplight') { Stoplight::Light.new('') {} }
-  b.report('calling stoplight')  { Stoplight::Light.new('') {}.run }
+  b.report('creating stoplight') { Stoplight('') {} }
+  b.report('calling stoplight')  { Stoplight('') {}.run }
 
   b.compare!
 end
