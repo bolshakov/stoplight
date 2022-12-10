92242cd09a9f03ee7dea0f7034b40eb9a5c28b94
Tema Bolshakov <either.free@gmail.com>
2022-12-11 00:55:44 +0100

    Test with_threshould and with_window_size together

--- spec/support/data_store/base/record_failures.rb
+++ spec/support/data_store/base/record_failures.rb
@@ -26,36 +26,45 @@ RSpec.shared_examples 'Stoplight::DataStore::Base#record_failure' do
     end
   end
 
-  context 'when the number of errors is bigger then threshold' do
-    before do
-      light.with_threshold(1)
+  shared_examples 'with_threshold' do
+    context 'when the number of errors is bigger then threshold' do
+      before do
+        light.with_threshold(1)
 
-      data_store.record_failure(light, failure)
-    end
+        data_store.record_failure(light, failure)
+      end
 
-    it 'limits the number of stored failures' do
-      expect do
-        data_store.record_failure(light, other)
-      end.to change { data_store.get_failures(light) }
-        .from([failure])
-        .to([other])
+      it 'limits the number of stored failures' do
+        expect do
+          data_store.record_failure(light, other)
+        end.to change { data_store.get_failures(light) }
+          .from([failure])
+          .to([other])
+      end
     end
   end
 
-  context 'when error is outside of the window' do
+  it_behaves_like 'with_threshold'
+
+  context 'with window_size' do
     let(:window_size) { 3600 }
-    let(:older_failure) { Stoplight::Failure.new('class', 'message 3', Time.new - window_size - 1) }
 
     before do
       light.with_window_size(window_size)
     end
 
-    it 'stores failures only withing window length' do
-      data_store.record_failure(light, failure)
-      data_store.record_failure(light, other)
-      data_store.record_failure(light, older_failure)
+    context 'when error is outside of the window' do
+      let(:older_failure) { Stoplight::Failure.new('class', 'message 3', Time.new - window_size - 1) }
 
-      expect(data_store.get_failures(light)).to eq([other, failure])
+      it 'stores failures only withing window length' do
+        data_store.record_failure(light, failure)
+        data_store.record_failure(light, other)
+        data_store.record_failure(light, older_failure)
+
+        expect(data_store.get_failures(light)).to eq([other, failure])
+      end
     end
+
+    it_behaves_like 'with_threshold'
   end
 end
