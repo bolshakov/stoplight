2ab84bd10b549ac3ba21d6e0bad602adedcbe7ec
Tëma Bolshakov <either.free@gmail.com>
2025-04-06 18:52:01 +0200

    Remove AVOID_RESCUING constant

--- lib/stoplight/error.rb
+++ lib/stoplight/error.rb
@@ -2,15 +2,6 @@
 
 module Stoplight
   module Error
-    AVOID_RESCUING = [
-      NoMemoryError,
-      ScriptError,
-      SecurityError,
-      SignalException,
-      SystemExit,
-      SystemStackError
-    ].freeze
-
     Base = Class.new(StandardError)
     IncorrectColor = Class.new(Base)
     RedLight = Class.new(Base)
