9bb02ff96426b5a0f05f268203e6d55873465f81
Tëma Bolshakov <either.free@gmail.com>
2025-04-09 21:24:36 +0200

    Test concurrent notifications

--- spec/support/data_store/base/with_deduplicated_notification.rb
+++ spec/support/data_store/base/with_deduplicated_notification.rb
@@ -24,4 +24,17 @@ RSpec.shared_examples 'Stoplight::DataStore::Base#with_deduplicated_notification
       end.to yield_control
     end
   end
+
+  context 'when handling concurrent access' do
+    it 'ensures atomicity under concurrent conditions' do
+      expect do |b|
+        threads = 100.times.map do
+          Thread.new do
+            data_store.with_deduplicated_notification(config, Stoplight::Color::GREEN, Stoplight::Color::RED, &b)
+          end
+        end
+        threads.each(&:join)
+      end.to yield_control.once
+    end
+  end
 end
