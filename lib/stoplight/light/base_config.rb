74d188cf918fa0b86fa4a304179cc62cb2aeeee5
Tëma Bolshakov <either.free@gmail.com>
2025-04-13 09:47:00 +0200

    Remove dead code

--- lib/stoplight/light/base_config.rb
+++ lib/stoplight/light/base_config.rb
@@ -16,11 +16,7 @@ module Stoplight
       attribute? :window_size, (Types::Integer | Types::Float)
       attribute? :tracked_errors, Types::Array.of(Types::TrackedError)
       attribute? :skipped_errors, Types::Array.of(Types::SkippedError).constructor { |value|
-        if value == Dry::Core::Undefined
-          Stoplight::Default::SKIPPED_ERRORS
-        else
-          Set[*value, *Stoplight::Default::SKIPPED_ERRORS].to_a
-        end
+        Set[*value, *Stoplight::Default::SKIPPED_ERRORS].to_a
       }
     end
   end
