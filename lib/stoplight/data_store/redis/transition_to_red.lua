3b6bd12a0e91645a1fc2c5f104f8c8c005f82833
Tëma Bolshakov <either.free@gmail.com>
2025-05-19 11:45:09 +0200

    Simplify transition to red script

--- lib/stoplight/data_store/redis/transition_to_red.lua
+++ lib/stoplight/data_store/redis/transition_to_red.lua
@@ -5,11 +5,6 @@ local recovery_scheduled_after_ts = tonumber(ARGV[2])
 --  1 if the field is a new field in the hash and the value was set
 local became_red = redis.call('HSETNX', meta_key, 'breached_at', current_ts)
 
-if became_red == 1 then
-  redis.call('HSET', meta_key, 'recovery_scheduled_after', recovery_scheduled_after_ts)
-else
-  -- Remove?
-  redis.call('HSET', meta_key, 'recovery_scheduled_after', recovery_scheduled_after_ts)
-end
+redis.call('HSET', meta_key, 'recovery_scheduled_after', recovery_scheduled_after_ts)
 redis.call("HDEL", meta_key, "recovery_started_at", "recovered_at")
 return became_red
