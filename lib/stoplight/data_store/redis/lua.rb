6ee027ee7e5c66b1dc125b3251c2c0b69d4e0c19
Tëma Bolshakov <either.free@gmail.com>
2025-05-26 17:29:29 +0200

    Add read_lua_file helper

--- lib/stoplight/data_store/redis/lua.rb
+++ lib/stoplight/data_store/redis/lua.rb
@@ -5,12 +5,18 @@ module Stoplight
     class Redis
       # @api private
       module Lua
-        RECORD_FAILURE = File.read(File.join(__dir__, "record_failure.lua"))
-        RECORD_SUCCESS = File.read(File.join(__dir__, "record_success.lua"))
-        GET_METADATA = File.read(File.join(__dir__, "get_metadata.lua"))
-        TRANSITION_TO_YELLOW = File.read(File.join(__dir__, "transition_to_yellow.lua"))
-        TRANSITION_TO_RED = File.read(File.join(__dir__, "transition_to_red.lua"))
-        TRANSITION_TO_GREEN = File.read(File.join(__dir__, "transition_to_green.lua"))
+        class << self
+          def read_lua_file(name_without_extension)
+            File.read(File.join(__dir__, "#{name_without_extension}.lua"))
+          end
+        end
+
+        RECORD_FAILURE = read_lua_file("record_failure")
+        RECORD_SUCCESS = read_lua_file("record_success")
+        GET_METADATA = read_lua_file("get_metadata")
+        TRANSITION_TO_YELLOW = read_lua_file("transition_to_yellow")
+        TRANSITION_TO_RED = read_lua_file("transition_to_red")
+        TRANSITION_TO_GREEN = read_lua_file("transition_to_green")
       end
     end
   end
