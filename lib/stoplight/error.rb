25c27821c0a27c2ed42b8e35b770fdad49d43541
lokideos <archmage@hey.com>
2025-10-21 15:01:52 +0600

    feat: Expose cool_off_time and retry_after on RedLight error

--- lib/stoplight/error.rb
+++ lib/stoplight/error.rb
@@ -5,6 +5,18 @@ module Stoplight
     Base = Class.new(StandardError)
     ConfigurationError = Class.new(Base)
     IncorrectColor = Class.new(Base)
-    RedLight = Class.new(Base)
+    class RedLight < Base
+      attr_reader :light_name      # The circuit breaker's name
+      attr_reader :cool_off_time   # Cool-off period in seconds
+      attr_reader :retry_after     # Absolute Time when recovery will be attempted
+
+      def initialize(light_name, cool_off_time:, retry_after:)
+        @light_name = light_name
+        @cool_off_time = cool_off_time
+        @retry_after = retry_after
+
+        super(light_name)
+      end
+    end
   end
 end
