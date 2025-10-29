2fef10c05a2be1c938e31b8b229557a4c342561a
Tëma Bolshakov <either.free@gmail.com>
2025-10-29 13:19:13 +0100

    Update lib/stoplight/wiring/default_configuration.rb
    Co-authored-by: George Asfour <archmage@hey.com>

--- lib/stoplight/wiring/default_configuration.rb
+++ lib/stoplight/wiring/default_configuration.rb
@@ -37,7 +37,7 @@ module Stoplight
       attr_accessor :notifiers
 
       # @!attribute [rw] data_store
-      #   @return [Stoplight::DataStore::Base] The default data store instance.
+      #   @return [Stoplight::Domain::DataStore] The default data store instance.
       attr_accessor :data_store
 
       # @!attribute [w] traffic_control
