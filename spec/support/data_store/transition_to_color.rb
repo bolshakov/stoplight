114b999971bebae90e01d2667351cbf6d1393055
Tëma Bolshakov <either.free@gmail.com>
2025-12-08 10:44:54 +0100

    Implement Stoplight::Infrastructure::Storage::Memory::State

--- spec/support/data_store/transition_to_color.rb
+++ spec/support/data_store/transition_to_color.rb
@@ -3,6 +3,26 @@
 RSpec.shared_examples "Stoplight::Domain::DataStore#transition_to_color" do
   let(:current_time) { Time.at(Time.now.to_i) }
 
+  shared_examples "thread safe" do |color|
+    let(:thread_count) { 50 }
+    let(:barrier) { Concurrent::CyclicBarrier.new(thread_count) }
+    let(:transition_results) { Concurrent::Array.new }
+
+    it "returns true exactly once across concurrent calls" do
+      threads = thread_count.times.map do
+        Thread.new do
+          barrier.wait  # All threads release simultaneously
+          transition_results << transition_to_color(color)
+        end
+      end
+
+      threads.each(&:join)
+
+      expect(transition_results.count(true)).to eq(1)
+      expect(transition_results.count(false)).to eq(thread_count - 1)
+    end
+  end
+
   context "when transitioning to GREEN" do
     context "when the color is already GREEN" do
       before do
@@ -30,6 +50,8 @@ RSpec.shared_examples "Stoplight::Domain::DataStore#transition_to_color" do
           recovery_scheduled_after: nil
         )
       end
+
+      it_behaves_like "thread safe", Stoplight::Color::GREEN
     end
   end
 
@@ -68,6 +90,8 @@ RSpec.shared_examples "Stoplight::Domain::DataStore#transition_to_color" do
           expect(state_snapshot).to have_attributes(recovery_started_at: nil)
         end
       end
+
+      it_behaves_like "thread safe", Stoplight::Color::YELLOW
     end
   end
 
@@ -94,7 +118,7 @@ RSpec.shared_examples "Stoplight::Domain::DataStore#transition_to_color" do
           end
         end.to change { state_snapshot }
           .from(have_attributes(breached_at: nil, recovery_scheduled_after: nil))
-          .to(have_attributes(breached_at: current_time, recovery_scheduled_after: current_time + config.cool_off_time))
+          .to(have_attributes(breached_at: current_time, recovery_scheduled_after: current_time + cool_off_time))
       end
 
       context "when cleared" do
@@ -106,6 +130,8 @@ RSpec.shared_examples "Stoplight::Domain::DataStore#transition_to_color" do
           expect(state_snapshot).to have_attributes(breached_at: nil, recovery_scheduled_after: nil)
         end
       end
+
+      it_behaves_like "thread safe", Stoplight::Color::RED
     end
 
     context "when the color is GREEN" do
@@ -122,7 +148,7 @@ RSpec.shared_examples "Stoplight::Domain::DataStore#transition_to_color" do
           end
         end.to change { state_snapshot }
           .from(have_attributes(breached_at: nil, recovery_scheduled_after: nil))
-          .to(have_attributes(breached_at: current_time, recovery_scheduled_after: current_time + config.cool_off_time))
+          .to(have_attributes(breached_at: current_time, recovery_scheduled_after: current_time + cool_off_time))
       end
 
       context "when cleared" do
@@ -134,6 +160,8 @@ RSpec.shared_examples "Stoplight::Domain::DataStore#transition_to_color" do
           expect(state_snapshot).to have_attributes(breached_at: nil, recovery_scheduled_after: nil)
         end
       end
+
+      it_behaves_like "thread safe", Stoplight::Color::RED
     end
 
     context "when transitioning to an invalid color" do
