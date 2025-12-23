06a1b096379c38d763b42ec7ce3049e79bb124dc
Tëma Bolshakov <either.free@gmail.com>
2025-12-23 22:25:29 +0100

    Add recovery metrics docs

--- lib/stoplight/infrastructure/storage/memory/recovery_metrics.rb
+++ lib/stoplight/infrastructure/storage/memory/recovery_metrics.rb
@@ -4,6 +4,11 @@ module Stoplight
   module Infrastructure
     module Storage
       module Memory
+        # When a circuit is RED (open), Stoplight periodically sends "recovery probes"
+        # to test whether the protected service has recovered. These test requests have
+        # different semantics than normal requests and their metrics are tracked separately.
+        #
+        # @see Stoplight::Domain::Storage::Metrics
         class RecoveryMetrics < UnboundedMetrics
         end
       end
--- lib/stoplight/infrastructure/storage/redis/recovery_metrics.rb
+++ lib/stoplight/infrastructure/storage/redis/recovery_metrics.rb
@@ -4,6 +4,11 @@ module Stoplight
   module Infrastructure
     module Storage
       module Redis
+        # When a circuit is RED (open), Stoplight periodically sends "recovery probes"
+        # to test whether the protected service has recovered. These test requests have
+        # different semantics than normal requests and their metrics are tracked separately.
+        #
+        # @see Stoplight::Domain::Storage::Metrics
         class RecoveryMetrics < UnboundedMetrics
           def initialize(redis:, scripting:, key_space:, clock:)
             super
