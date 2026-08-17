d0c997cb6ef63bea09a57612fdcdda6f7eb8fc68
Nebiyu Talefe <nebiyuelias1@gmail.com>
2026-08-17 22:37:58 +0800

    Nest spec context

--- spec/unit/stoplight/domain/state_snapshot_spec.rb
+++ spec/unit/stoplight/domain/state_snapshot_spec.rb
@@ -46,17 +46,17 @@ RSpec.describe Stoplight::Domain::StateSnapshot do
       it { is_expected.to be(Stoplight::Color::YELLOW) }
     end
 
-    context "when threshold is breached and recovery is in the future" do
-      let(:breached_at) { time - 3 }
-      let(:recovery_started_at) { time + 3 }
-
-      it { is_expected.to be(Stoplight::Color::RED) }
-    end
 
     context "when threshold breached" do
       let(:breached_at) { time - 3 }
 
       it { is_expected.to be(Stoplight::Color::RED) }
+
+      context "when recovery is in the future" do
+        let(:recovery_started_at) { time + 3 }
+
+        it { is_expected.to be(Stoplight::Color::RED) }
+      end
     end
   end
 end
