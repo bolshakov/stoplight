616abcae3cfe84f204f4e8a19ee9820506bc645c
Tëma Bolshakov <either.free@gmail.com>
2026-08-27 22:26:17 +0200

    Use hincrby instead of manual increment

--- lib/stoplight/infrastructure/redis/storage/window_metrics/record_failure.lua
+++ lib/stoplight/infrastructure/redis/storage/window_metrics/record_failure.lua
@@ -17,28 +17,25 @@ redis.call('ZREMRANGEBYSCORE', failures_key, '-inf', '(' .. window_start_ts_str)
 redis.call('EXPIRE', failures_key, zset_ttl)
 
 -- Update metadata
-local meta = redis.call('HMGET', metrics_key, 'last_error_at', 'consecutive_errors')
+local meta = redis.call('HMGET', metrics_key, 'last_error_at')
 local prev_failure_ts = tonumber(meta[1])
-local prev_consecutive_errors = tonumber(meta[2])
 
-local consecutive_errors = (prev_consecutive_errors or 0) + 1
-local consecutive_successes = 0
 local last_error_json_result
+
+local consecutive_errors = redis.call("HINCRBY", metrics_key, "consecutive_errors", 1)
 
 if not prev_failure_ts or failure_ts > prev_failure_ts then
   redis.call(
     'HSET', metrics_key,
     'last_error_at', failure_ts,
     'last_error_json', failure_json,
-    'consecutive_errors', consecutive_errors,
-    'consecutive_successes', consecutive_successes
+    'consecutive_successes', 0
   )
   last_error_json_result = failure_json
 else
   redis.call(
     'HSET', metrics_key,
-    'consecutive_errors', consecutive_errors,
-    'consecutive_successes', consecutive_successes
+    'consecutive_successes', 0
   )
   last_error_json_result = redis.call('HGET', metrics_key, 'last_error_json')
 end
@@ -49,4 +46,4 @@ local successes = tonumber(redis.call('ZCOUNT', successes_key, window_start_ts_s
 local errors = tonumber(redis.call('ZCOUNT', failures_key, window_start_ts_str, failure_ts_str))
 local last_success_at = redis.call('HGET', metrics_key, 'last_success_at')
 
-return {successes, errors, last_success_at, last_error_json_result, consecutive_errors, consecutive_successes}
+return {successes, errors, last_success_at, last_error_json_result, consecutive_errors, 0}
--- lib/stoplight/infrastructure/redis/storage/window_metrics/record_success.lua
+++ lib/stoplight/infrastructure/redis/storage/window_metrics/record_success.lua
@@ -14,22 +14,21 @@ redis.call('ZREMRANGEBYSCORE', successes_key, '-inf', '(' .. window_start_ts_str
 redis.call('EXPIRE', successes_key, zset_ttl)
 
 -- Update metadata
-local meta = redis.call('HMGET', metrics_key, 'last_success_at', 'consecutive_successes')
+local meta = redis.call('HMGET', metrics_key, 'last_success_at')
 local prev_success_ts = tonumber(meta[1])
-local prev_consecutive_successes = tonumber(meta[2])
+
+redis.call("HINCRBY", metrics_key, "consecutive_successes", 1)
 
 if not prev_success_ts or request_ts > prev_success_ts then
   redis.call(
     'HSET', metrics_key,
     'last_success_at', request_ts,
-    'consecutive_errors', 0,
-    'consecutive_successes', (prev_consecutive_successes or 0) + 1
+    'consecutive_errors', 0
   )
 else
   redis.call(
     'HSET', metrics_key,
-    'consecutive_errors', 0,
-    'consecutive_successes', (prev_consecutive_successes or 0) + 1
+    'consecutive_errors', 0
   )
 end
 
