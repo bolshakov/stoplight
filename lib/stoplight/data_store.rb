9cd7bda729965b77686bfadc7034b3a422553b0d
Tëma Bolshakov <either.free@gmail.com>
2026-09-06 10:11:59 +0200

    Update UPGRADING.md (#924)
    * Remove the no-op warn_on_clock_skew data store option
    
    Clock skew detection went away when state transitions and metrics snapshots
    moved onto Redis's own clock. The keyword outlived it as a no-op that stored a
    value nothing read, so passing it promised a warning that could never fire.
    
    * Add updating instructions

--- lib/stoplight/data_store.rb
+++ lib/stoplight/data_store.rb
@@ -12,16 +12,8 @@ module Stoplight
       #   @return [::Redis, ConnectionPool<::Redis>]
       attr_reader :redis
 
-      # @!attribute warn_on_clock_skew
-      #   FIXME: seems this functionality was removed
-      #   @return [Boolean]
-      attr_reader :warn_on_clock_skew
-
       # @param redis [::Redis, ConnectionPool<::Redis>]
-      # @param warn_on_clock_skew [Boolean] (true) Whether to warn about clock skew between Redis and
-      #   the application server
-      def initialize(redis, warn_on_clock_skew: true)
-        @warn_on_clock_skew = warn_on_clock_skew
+      def initialize(redis)
         @redis = redis
       end
 
